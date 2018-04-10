b71636b73fb1efee997fedcb4bb48dba2298c974
Keisuke Kan <9renpoto@gmail.com>
2018-04-10 12:04:09 +0900

    feat(__tests__) Added snapshot test (#5)
    * feat(__tests__) Added snapshot test
    
    * docs(readme) Added badge
    
    * chore(travis) Added notification
    
    * fix(package) post coverage
    
    * refactor(components) remove __tests__ dir
    
    * fix(packae) revert test regex

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,10 @@
 import { configure, addDecorator } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 
-const req = require.context("../src/components", true, /.stories.tsx?$/);
+const req = require.context("../components", true, /.stories.tsx?$/);
 const loadStories = () => req.keys().forEach(filename => req(filename));
 
-import "../src/components/globalStyles";
+import "../components/globalStyles";
 
 addDecorator(withKnobs);
 configure(loadStories, module);
