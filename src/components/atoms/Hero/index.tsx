7131bb95355566e3fdc1948816ec2ae4f20598c6
SaeMakino <sae1116.m@gmail.com>
2018-04-11 18:50:50 +0900

    見様見真似でやってみた

--- src/components/atoms/Hero/index.tsx
+++ src/components/atoms/Hero/index.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const Hero = styled.input`
+const Hero = styled.duv`
   width: 100%;
   min-height: 7.5rem;
   margin-bottom: 3.125rem;
@@ -9,4 +9,12 @@ const Hero = styled.input`
   background: linear-gradient(90deg, rgba(235, 46, 62, 0.6), rgba(249, 180, 45, 0.6));
 `;
 
+const HeroInner = styled.div`
+  display: flex;
+  position: absolute;
+  bottom: -30%;
+  left: 2rem;
+`;
+
 export default Hero;
+export default HeroInner;
--- src/components/atoms/Profile/index.tsx
+++ src/components/atoms/Profile/index.tsx
@@ -1,6 +1,19 @@
 import styled from "styled-components";
 
-const Profile = styled.input`
+const ProfileImg = styled.div`
+  width: 100%;
+  min-height: 7.5rem;
+  margin-bottom: 3.125rem;
+  position: relative;
+  background: #eb2e3e;
+  background: linear-gradient(90deg, rgba(235, 46, 62, 0.6), rgba(249, 180, 45, 0.6));
+
+  img {
+    border-radius: 50%;
+  }
+`;
+
+const ProfileName = styled.p`
   width: 100%;
   min-height: 7.5rem;
   margin-bottom: 3.125rem;
@@ -9,4 +22,5 @@ const Profile = styled.input`
   background: linear-gradient(90deg, rgba(235, 46, 62, 0.6), rgba(249, 180, 45, 0.6));
 `;
 
-export default Profile;
+export default ProfileImg;
+export default ProfileName;
