4ae052faff37cb821a006f2d680948a61ae77486
Keisuke Kan <9renpoto@gmail.com>
2018-04-11 18:22:02 +0900

    fix(package) start-storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,16 @@
+const { CheckerPlugin } = require("awesome-typescript-loader");
+
 module.exports = {
   module: {
     rules: [
       {
         test: /\.tsx?$/,
+        exclude: /node_modules/,
         loader: "awesome-typescript-loader"
       }
     ]
   },
+  plugins: [new CheckerPlugin()],
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"]
   }
