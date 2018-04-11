896c3795853a15d4fe54674919304141a3aafd1d
Keisuke Kan <9renpoto@gmail.com>
2018-04-11 18:54:31 +0900

    fix(package) start-storybook (#20)
    * fix(package) start-storybook
    
    * fix(package) remove unused
    
    * chore(components) update snapshot
    
    * fix(tslint) remove unnecessary options

--- components/atoms/Input/index.stories.tsx
+++ components/atoms/Input/index.stories.tsx
@@ -1,6 +1,8 @@
-import { storiesOf } from "@storybook/react";
-import * as React from "react";
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
 
-import Input from ".";
+import Input from '.'
 
-storiesOf("Atoms.Input", module).add("default", () => <Input />);
+storiesOf('Atoms.Input', module).add('default', () => (
+  <Input placeholder='hello' />
+))
