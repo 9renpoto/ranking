119a33687bc30d6cf1129b841453a2beadcd05f0
Keisuke Kan <9renpoto@gmail.com>
2018-04-11 19:13:14 +0900

    refactor(components) move files

--- components/atoms/Hero/index.tsx
+++ components/atoms/Hero/index.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const Hero = styled.duv`
   width: 100%;
@@ -6,15 +6,19 @@ const Hero = styled.duv`
   margin-bottom: 3.125rem;
   position: relative;
   background: #eb2e3e;
-  background: linear-gradient(90deg, rgba(235, 46, 62, 0.6), rgba(249, 180, 45, 0.6));
-`;
+  background: linear-gradient(
+    90deg,
+    rgba(235, 46, 62, 0.6),
+    rgba(249, 180, 45, 0.6)
+  );
+`
 
 const HeroInner = styled.div`
   display: flex;
   position: absolute;
   bottom: -30%;
   left: 2rem;
-`;
+`
 
-export default Hero;
-export default HeroInner;
+export default Hero
+export default HeroInner
