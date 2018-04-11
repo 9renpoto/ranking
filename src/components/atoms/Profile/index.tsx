e04cf7b7b64b59d640dbffba9a304005649ad206
SaeMakino <sae1116.m@gmail.com>
2018-04-11 14:17:29 +0900

    マイページ、評価入力html作成

---
+++ src/components/atoms/Profile/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Profile = styled.input`
+  width: 100%;
+  min-height: 7.5rem;
+  margin-bottom: 3.125rem;
+  position: relative;
+  background: #eb2e3e;
+  background: linear-gradient(90deg, rgba(235, 46, 62, 0.6), rgba(249, 180, 45, 0.6));
+`;
+
+export default Profile;
