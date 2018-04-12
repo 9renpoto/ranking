0671a2e6f27f91b766153d0c69c92a191c56bac5
SaeMakino <sae1116.m@gmail.com>
2018-04-12 15:47:37 +0900

    stories作成

---
+++ components/atoms/Textarea/index.stories.tsx
@@ -0,0 +1,8 @@
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+
+import Textarea from '.'
+
+storiesOf('Atoms.Textarea', module).add('default', () => (
+  <Textarea placeholder='hello' />
+))
