f7007bec103ce847fb0559d6c92cbb4316c4c217
SaeMakino <sae1116.m@gmail.com>
2018-04-20 16:27:22 +0900

    評価画面作成

--- components/a/Profile/index.stories.tsx
+++ components/a/Profile/index.stories.tsx
@@ -2,19 +2,27 @@ import { storiesOf } from '@storybook/react'
 import { image, random } from 'faker'
 import * as React from 'react'
 
-import { Img, Name } from '.'
+import { Img, Inner, Name } from '.'
 
 export const DefaultProfile = () => (
   <React.Fragment>
-    <Img>
-      <img src={image.imageUrl(300, 300)} alt={random.word()} />
-    </Img>
-    <Name>{random.word()}</Name>
+    <Inner>
+      <Img>
+        <img src={image.imageUrl(300, 300)} alt={random.word()} />
+      </Img>
+      <Name>{random.word()}</Name>
+    </Inner>
   </React.Fragment>
 )
 
 storiesOf('Atoms.Profile', module).add('default', () => (
-  <div style={{ 'background-color': '#1111' }}>
+  <div
+    style={{
+      'background-color': '#1111',
+      height: '7.5rem',
+      position: 'relative'
+    }}
+  >
     <DefaultProfile />
   </div>
 ))
