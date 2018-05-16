7f69c84c99778da9821d3d679222bc7a1d8dc77f
taka-sho <takasho530000@gmail.com>
2018-05-16 16:08:29 +0900

    chore(pages/{index | mypage}.tsx) do not use React.Component

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,6 +1,4 @@
 import { image, name, random } from 'faker'
-import * as firebase from 'firebase'
-import * as React from 'react'
 import { Avatar } from '../components/a/Avatar'
 import { Box, Inner as BoxInner } from '../components/a/Box'
 import { Hero } from '../components/a/Hero'
@@ -22,6 +20,10 @@ import { Inner as HeroInner } from '../components/m/Hero'
 import { Navbar } from '../components/m/Navbar'
 import { Rank } from '../components/m/Ranking'
 
+import * as firebase from 'firebase'
+import * as React from 'react'
+import routes from '../app/routes'
+
 type Props = {
   url: {
     query: {
@@ -32,81 +34,75 @@ type Props = {
   }
 }
 
-export default class Mypage extends React.Component<Props> {
-  constructor (props: Props) {
-    super(props)
+export default ({
+  url: {
+    query: { orgId, memberId, photoURL }
   }
-  render () {
-    return (
-      <Wrapper>
-        <Navbar
-          orgId={this.props.url.query.orgId}
-          memberId={this.props.url.query.memberId}
-        />
-        <Main>
-          <Hero>
-            <HeroInner>
-              <Avatar isCircle width={4.688} height={4.688}>
-                <img src={this.props.url.query.photoURL} alt={random.word()} />
+}: Props) => (
+  <Wrapper>
+    <Navbar orgId={orgId} memberId={memberId} />
+    <Main>
+      <Hero>
+        <HeroInner>
+          <Avatar isCircle width={4.688} height={4.688}>
+            <img src={photoURL} alt={random.word()} />
+          </Avatar>
+          <Name white padding={1}>
+            {memberId}
+          </Name>
+        </HeroInner>
+      </Hero>
+      <Box>
+        <BoxInner>
+          <Range>
+            <RangeInner>
+              <RangeValuation>
+                <p>とても悪い</p>
+                <p>とても良い</p>
+              </RangeValuation>
+              <RangeBar>
+                <RangeBarThumb style={{ left: '50%' }} />
+                <RangeValue style={{ left: '50%' }}>3</RangeValue>
+              </RangeBar>
+            </RangeInner>
+          </Range>
+        </BoxInner>
+        <BoxInner>
+          <ResultRank>
+            <ResultRankInner>
+              <span>50人中</span>
+              10位
+            </ResultRankInner>
+          </ResultRank>
+          <ResultComment>{random.word()}</ResultComment>
+        </BoxInner>
+      </Box>
+      <Box>
+        <BoxInner>
+          <Rank>
+            <span>{random.number()}</span>
+            <RankBar style={{ width: `${random.number({ max: 100 })}%` }}>
+              <Avatar width={0.938} height={0.938}>
+                <img src={image.imageUrl(300, 300)} alt={random.word()} />
               </Avatar>
-              <Name white padding={1}>
-                {this.props.url.query.memberId}
+              <Name white padding={0}>
+                {random.word()}
               </Name>
-            </HeroInner>
-          </Hero>
-          <Box>
-            <BoxInner>
-              <Range>
-                <RangeInner>
-                  <RangeValuation>
-                    <p>とても悪い</p>
-                    <p>とても良い</p>
-                  </RangeValuation>
-                  <RangeBar>
-                    <RangeBarThumb style={{ left: '50%' }} />
-                    <RangeValue style={{ left: '50%' }}>3</RangeValue>
-                  </RangeBar>
-                </RangeInner>
-              </Range>
-            </BoxInner>
-            <BoxInner>
-              <ResultRank>
-                <ResultRankInner>
-                  <span>50人中</span>
-                  10位
-                </ResultRankInner>
-              </ResultRank>
-              <ResultComment>{random.word()}</ResultComment>
-            </BoxInner>
-          </Box>
-          <Box>
-            <BoxInner>
-              <Rank>
-                <span>{random.number()}</span>
-                <RankBar style={{ width: `${random.number({ max: 100 })}%` }}>
-                  <Avatar width={0.938} height={0.938}>
-                    <img src={image.imageUrl(300, 300)} alt={random.word()} />
-                  </Avatar>
-                  <Name white padding={0}>
-                    {random.word()}
-                  </Name>
-                </RankBar>
-              </Rank>
-              <Rank>
-                <span>{random.number()}</span>
-                <RankBar style={{ width: `${random.number({ max: 100 })}%` }}>
-                  <Avatar width={0.938} height={0.938}>
-                    <img src={image.imageUrl(300, 300)} alt={random.word()} />
-                  </Avatar>
-                  <Name white padding={0}>
-                    {random.word()}
-                  </Name>
-                </RankBar>
-              </Rank>
-            </BoxInner>
-          </Box>
-        </Main>
-      </Wrapper>
-    )
-  }
-}
+            </RankBar>
+          </Rank>
+          <Rank>
+            <span>{random.number()}</span>
+            <RankBar style={{ width: `${random.number({ max: 100 })}%` }}>
+              <Avatar width={0.938} height={0.938}>
+                <img src={image.imageUrl(300, 300)} alt={random.word()} />
+              </Avatar>
+              <Name white padding={0}>
+                {random.word()}
+              </Name>
+            </RankBar>
+          </Rank>
+        </BoxInner>
+      </Box>
+    </Main>
+  </Wrapper>
+)
