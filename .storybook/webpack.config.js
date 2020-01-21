97d0a538eadd5b8e0289996500dd205dfc0569c0
Keisuke Kan <9renpoto@gmail.com>
2020-01-21 19:23:06 +0900

    fix: types

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,8 +1,24 @@
 module.exports = async ({ config: defaultConfig }) => {
   defaultConfig.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    exclude: /node_modules/,
-    loader: require('ts-loader')
+    test: /\.[j|t]sx?$/,
+    use: [
+      {
+        loader: require.resolve('babel-loader'),
+        options: {
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-optional-chaining',
+            'macros',
+            'lodash'
+          ],
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+            '@babel/preset-typescript'
+          ]
+        }
+      }
+    ]
   })
 
   defaultConfig.resolve.alias = Object.assign({}, defaultConfig.resolve.alias, {
