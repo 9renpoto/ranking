95f70493da0ff3bc5d9bbb1ac114b797abe4dd02
Keisuke Kan <9renpoto@gmail.com>
2020-02-09 11:03:25 +0900

    chore: use preact

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -15,5 +15,13 @@ module.exports = async ({ config: defaultConfig }) => {
 
   defaultConfig.resolve.extensions.push('.ts', '.tsx', '.jsx')
 
+  defaultConfig.resolve.alias = {
+    ...defaultConfig.resolve.alias,
+    react: 'preact/compat',
+    react$: 'preact/compat',
+    'react-dom': 'preact/compat',
+    'react-dom$': 'preact/compat'
+  }
+
   return defaultConfig
 }
