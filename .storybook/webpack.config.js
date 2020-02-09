9634c0e1775d68a3640299efae4120d4dac0a275
Keisuke Kan <9renpoto@gmail.com>
2020-02-09 11:00:27 +0900

    chore: use next/babel

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,31 +1,16 @@
+const $ = require('core-js/es/object/get-own-property-descriptor')
+console.warn($)
+
 module.exports = async ({ config: defaultConfig }) => {
   defaultConfig.module.rules.push({
     test: /\.[j|t]sx?$/,
+    exclude: [/\bcore-js\b/, /\bwebpack\/buildin\b/],
     use: [
       {
         loader: require.resolve('babel-loader'),
-        options: {
-          plugins: [
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-proposal-optional-chaining',
-            'macros',
-            'lodash'
-          ],
-          presets: [
-            '@babel/preset-env',
-            '@babel/preset-react',
-            '@babel/preset-typescript'
-          ]
-        }
+        options: require('../babel.config')
       }
     ]
-  })
-
-  defaultConfig.resolve.alias = Object.assign({}, defaultConfig.resolve.alias, {
-    react: 'preact/compat',
-    react$: 'preact/compat',
-    'react-dom': 'preact/compat',
-    'react-dom$': 'preact/compat'
   })
 
   defaultConfig.resolve.extensions.push('.ts', '.tsx', '.jsx')
